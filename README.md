244f40c1ffc54cc5c4702036010d304df0142142
florian935 <florian.martin63000@gmail.com>
2021-09-22 08:56:57 +0200

    Feat: demo of 2 differents languages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Angular i18n Demo
 
 This project is a demo of how to manage internationalization in Angular app.
+
+To build app with different locales, run `ng build --localize`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-i18n-demo';
+    minutes = 0;
+    gender = 'female';
+    logo = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+    increment(i: number): void {
+        this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+    }
+
+    male(): void {
+        this.gender = 'male';
+    }
+
+    female(): void {
+        this.gender = 'female';
+    }
+
+    other(): void {
+        this.gender = 'other';
+    }
 }
